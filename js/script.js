4ec159609379fab2d9efc95da567b9e37cda937f
Jason Gutierrez <jasong84@gmail.com>
2020-01-21 18:08:59 -0600

    Add hard coded element for student to dynamically create
    # |<----  Using a Maximum Of 50 Characters  ---->|
    
    
    # Explain why this change is being made
    # |<----   Try To Limit Each Line to a Maximum Of 72 Characters   ---->|
    
    # Provide links or keys to any relevant tickets, articles or other resources
    # Example: Github issue #23
    
    # --- COMMIT END ---
    # Type can be 
    #    feat     (new feature)
    #    fix      (bug fix)
    #    refactor (refactoring production code)
    #    style    (formatting, missing semi colons, etc; no code change)
    #    docs     (changes to documentation)
    #    test     (adding or refactoring tests; no production code change)
    #    chore    (updating grunt tasks etc; no production code change)
    # --------------------
    # Remember to
    #    Capitalize the subject line
    #    Use the imperative mood in the subject line
    #    Do not end the subject line with a period
    #    Separate subject from body with a blank line
    #    Use the body to explain what and why vs. how
    #    Can use multiple lines with "-" for bullet points in body
    # --------------------

--- js/script.js
+++ js/script.js
@@ -2,11 +2,52 @@ var xhr = new XMLHttpRequest();
 xhr.withCredentials = true;
 xhr.overrideMimeType("application/json")
 xhr.addEventListener("readystatechange", function() {
-  if(this.readyState === 4) {
-     console.log(this.responseText)
-  }
+    if (this.readyState === 4) {
+        console.log(this.responseText)
+        var elements = formatJSON(this.responseText) //?
+        displayElements(elements); // We have it assigned to a variable. use the variable.
+    }
 });
 
-xhr.open("GET", "PeriodicTable.json");
+xhr.open("GET", "http://localhost:3000/PeriodicTable.json");
 
 xhr.send();
+
+
+function formatJSON(jsonData) {
+    return JSON.parse(jsonData).elements.map(function(element) {
+        return ({
+            name: element.name,
+            number: element.number,
+            symbol: element.symbol,
+            category: element.category,
+            atomic_mass: element.atomic_mass,
+            melt: element.melt
+        })
+    })
+
+}
+
+
+function displayElements(elements) {
+    // This is strictly a style thing but keep the function name and () close together
+    // we will be receiving the array of objects from the formatJSON as the input.
+    // I'll add this bit for you but look at the index.html file so you can see why i'm adding this next line
+    var periodicTableParent = document.querySelector('#parent-table');
+    // This is the periodic table container where we will add the elements in.
+
+    // this is the element we will be making. watch the index.html file when we are done
+    var elementNode = document.createElement('div')
+        // we have our 'bare' element but we need some classes so you can see it
+    elementNode.classList = ["ba element pa1 bg-blue"]
+
+
+
+
+    periodicTableParent.insertAdjacentElement('beforeend', elementNode);
+
+}
+// Remember, nothing will happen because it ALL has to be inside the HTTP Request
+// anything outside of this will not work
+
+// displayElements(formatJSON);// <- Will not work. Needs to be on line 8
